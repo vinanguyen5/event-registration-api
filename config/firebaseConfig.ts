54dcbf6ef3ae116f797a4eaaedd5d8fcdebcb333
Vina <vnguyen5@acadmic.rrc.ca>
2026-06-05 20:21:52 -0500

    chore: configure firebase admin credentials

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-    admin.initializeApp();
+    admin.initializeApp({
+        credential: admin.credential.applicationDefault(),
+    });
 }
 
 export const db = admin.firestore();
