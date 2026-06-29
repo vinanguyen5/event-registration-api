d022550a48fb62348f1748190c6230af6de4c223
Vina <vnguyen5@acadmic.rrc.ca>
2026-06-29 17:31:42 -0500

    feat: add openapi generation script

---
+++ scripts/generate-openapi.ts
@@ -0,0 +1,8 @@
+﻿import fs from "fs";
+import { generateSwaggerSpec } from "../config/swaggerOptions";
+
+const specs = generateSwaggerSpec();
+
+fs.writeFileSync("./openapi.json", JSON.stringify(specs, null, 2));
+
+console.log("OpenAPI specification generated successfully!");
